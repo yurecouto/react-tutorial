0ef4c02b22bb7dbe52ab648d0d95caccea85a387
yurecouto <yure.couto@hotmail.com>
2022-04-09 12:56:27 -0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const DATA = [
+  { id: "todo-0", name: "Alimentar a Alpha e a Kiara", completed: false },
+];
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App tasks={DATA} />,
   </React.StrictMode>,
   document.getElementById('root')
 );
